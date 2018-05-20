89a66e789a0621a231c16c57d0a004da76aa429a
Obayashi Tatsunori <4210.tatu@gmail.com>
2018-05-20 05:07:24 +0000

    サーバー側でHTMLを修正する

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var server = http.createServer(
     (request,response)=>{
         fs.readFile('./index.html','UTF-8',
         (error,data)=>{
+            var content=data.
+                replace(/dummy_title/g, 'タイトルです').
+                replace(/dummy_content/g, 'これがコンテンツです。');
            response.writeHead(200,{'Content-Type': 'text/html'});
-           response.write(data);
+           response.write(content);
            response.end();
         });
     }
