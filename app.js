f7b9d02eaf8ad789dbb4eac18e1d44fcfbd0012c
Obayashi Tatsunori <4210.tatu@gmail.com>
2018-05-20 04:38:33 +0000

    app.jsとindex.htmlを更新

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 
 var server = http.createServer(
     (request,response)=>{
-        response.end('Hello Node.js!');
+        fs.readFile('./index.html','UTF-8',
+        (error,data)=>{
+           response.writeHead(200,{'Content-Type': 'text/html'});
+           response.write(data);
+           response.end();
+        });
     }
     );
 server.listen(8080);
+console.log('Server start!');
